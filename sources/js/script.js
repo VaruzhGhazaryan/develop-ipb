a08c5ca3195af47aaea32a8cd03edb9460d1790e
Varuzh <varuzh11@gmail.com>
2017-10-09 01:22:56 +0400

    added content design and some filters

--- sources/js/script.js
+++ sources/js/script.js
@@ -1,13 +1,18 @@
 "use strict"
 
 $(function(){
-    $('#cntByDay').niceSelect();
+    $('#cntByDay, #price-for-week').niceSelect();
     $(".form_datetime").datetimepicker({
         minView: 2, 
         pickTime: false,
         autoclose: true,
         format: "dd MM yyyy",
-        pickerPosition: 'bottom',
+        pickerPosition: 'bottom-right',
         language:'ru',
+    });
+    $('#price-filter-slide').slider({
+        formatter: function(value) {
+            return 'От: ' + value + ' руб.';
+        }
     });
 })

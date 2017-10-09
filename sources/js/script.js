6ce912134961e59fe249f647cd3b8cfe29f8b098
Varuzhan Ghazaryan <varuzh11@gmail.com>
2017-10-09 14:33:43 +0400

    complete design

--- sources/js/script.js
+++ sources/js/script.js
@@ -16,3 +16,13 @@ $(function(){
         }
     });
 })
+
+    // Init google maps
+    function initMap() {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 55.7391749, lng: 37.5998668},
+          zoom: 10,
+          disableDefaultUI: true
+        });
+    }
